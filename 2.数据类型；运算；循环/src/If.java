a44a63c444f1fee72c1de40762fbbeaff5819691
Wyxiao11111 <769210622@qq.com>
2018-01-05 18:58:44 +0800

    1

--- 2.数据类型；运算；循环/src/If.java
+++ 2.数据类型；运算；循环/src/If.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class If {
     public static void main(String args[]) {
         Scanner input = new Scanner(System.in);
-        double score = input.nextInt();            // 输入，整数一般用int，小数则用double
+        double score = input.nextDouble();            // 输入，整数一般用int，小数则用double
         if (score < 60.0) {						   // 条件判断
             System.out.println("小白的成绩！") ;
         } else if (score >= 60 && score <= 90) {		// 条件判断
